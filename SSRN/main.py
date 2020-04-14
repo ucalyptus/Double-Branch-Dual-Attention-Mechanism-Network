6b89be9806f869b84f94eb9cb4dcd00799a575de
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-04-14 13:23:37 +0530

    Update main.py

--- SSRN/main.py
+++ SSRN/main.py
@@ -39,7 +39,7 @@ CLASSES_NUM = max(gt)
 print('The class numbers of the HSI data is:', CLASSES_NUM)
 
 print('-----Importing Setting Parameters-----')
-ITER = 10
+ITER = 3
 PATCH_LENGTH = 3
 # number of training samples per class
 #lr, num_epochs, batch_size = 0.0001, 200, 32
