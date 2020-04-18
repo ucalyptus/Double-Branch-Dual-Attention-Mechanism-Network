da343eb810b553043442d83814bcb2d05fc73419
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-04-18 21:08:47 +0530

    Update generate_pic.py

--- global_module/generate_pic.py
+++ global_module/generate_pic.py
@@ -168,7 +168,7 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
 
     x_test = x_test_all[:-VAL_SIZE]
     y_test = y_test[:-VAL_SIZE]
-    
+    print(x_test.shape,y_test.shape)
     # x_train.shape is TRAIN_SIZE,5,5,25,1
     
     
