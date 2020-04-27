092e4e851b557d5e653fac6e47cc4e4cd32d4599
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-04-27 23:51:08 +0530

    Update minmax.py

--- global_module/minmax.py
+++ global_module/minmax.py
@@ -29,91 +29,43 @@ class MinMaxCNNLayer(nn.Module):
     conv3 = torch.cat((conv1,conv2),dim=1)
     return conv3
     
-class MinMaxCNNNetwork(nn.Module):
-  def __init__(self):
-    super(MinMaxCNNNetwork,self).__init__()
-    self.minmax1 = MinMaxCNNLayer(3,32,5,1,2)
-    self.minmax2 = MinMaxCNNLayer(64,32,5,1,2)
-    self.minmax3 = MinMaxCNNLayer(64,64,5,1,2)
-    self.pool = nn.MaxPool2d(kernel_size=3,stride=2)
-    self.relu = nn.ReLU()
-    self.fc1 = nn.Linear(6272,64)
-    self.fc2 = nn.Linear(64,10)
-  def forward(self,x):
-    out = self.relu(self.minmax1(x))
-    out = self.pool(out)
-    out = self.relu(self.minmax2(out))
-    out = self.pool(out)
-    out = self.relu(self.minmax3(out))
-    out = out.view(-1,6272)
-    out = self.relu(self.fc1(out))
-    out = self.fc2(out)
-    return F.log_softmax(out,dim=1)
-
-
-#minmaxcnn = MinMaxCNNNetwork().to(device)
-
-
-class Residual(nn.Module): 
-    def __init__(self, in_channels, out_channels, kernel_size, padding, use_1x1conv=False, stride=1):
-        super(Residual, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels,
-                      kernel_size=kernel_size, padding=padding, stride=stride),
-            nn.ReLU()
-        )
-        self.conv2 = nn.Conv3d(out_channels, out_channels,
-                               kernel_size=kernel_size, padding=padding,stride=stride)
-        if use_1x1conv:
-            self.conv3 = nn.Conv3d(in_channels, out_channels, kernel_size=1, stride=stride)
-        else:
-            self.conv3 = None
-        self.bn1 = nn.BatchNorm3d(out_channels)
-        self.bn2 = nn.BatchNorm3d(out_channels)
-
-    def forward(self, X):
-        Y = F.relu(self.bn1(self.conv1(X)))
-        Y = self.bn2(self.conv2(Y))
-        if self.conv3:
-            X = self.conv3(X)
-        return F.relu(Y + X)
-
-
-class SSRN_network(nn.Module):
+class SSRN_MINMAX(nn.Module):
     def __init__(self, band, classes):
-        super(SSRN_network, self).__init__()
-        self.name = 'SSRN'
-        self.conv1 = nn.Conv3d(in_channels=1, out_channels=24,
-                                kernel_size=(1, 1, 7), stride=(1, 1, 2))
+        super(SSRN_MINMAX, self).__init__()
+        self.name = 'SSRN_MINMAX'
+        self.conv1 = MinMaxCNNLayer(1,24,(1,1,7),(1,1,2),(0,0,0))
         self.batch_norm1 = nn.Sequential(
-            nn.BatchNorm3d(24, eps=0.001, momentum=0.1, affine=True),  # 动量默认值为0.1
+            nn.BatchNorm3d(24*2, eps=0.001, momentum=0.1, affine=True), 
             nn.ReLU(inplace=True)
         )
 
-        self.res_net1 = Residual(24, 24, (1, 1, 7), (0, 0, 3))
-        self.res_net2 = Residual(24, 24, (1, 1, 7), (0, 0, 3))
-        self.res_net3 = Residual(24, 24, (3, 3, 1), (1, 1, 0))
-        self.res_net4 = Residual(24, 24, (3, 3, 1), (1, 1, 0))
+        self.res_net1 = Residual(24*2, 24*2, (1, 1, 7), (0, 0, 3))
+        self.res_net2 = Residual(24*2, 24*2, (1, 1, 7), (0, 0, 3))
+        self.res_net3 = Residual(24*2, 24*2, (3, 3, 1), (1, 1, 0))
+        self.res_net4 = Residual(24*2, 24*2, (3, 3, 1), (1, 1, 0))
 
         kernel_3d = math.ceil((band - 6) / 2)
 
-        self.conv2 = nn.Conv3d(in_channels=24, out_channels=128, padding=(0, 0, 0),
-                               kernel_size=(1, 1, kernel_3d), stride=(1, 1, 1))
+
+        self.conv2 = MinMaxCNNLayer(24*2, 128, paddinglength=(0, 0, 0),
+                               kernelsize=(1, 1, kernel_3d), stride=(1, 1, 1))
+
         self.batch_norm2 = nn.Sequential(
-            nn.BatchNorm3d(128, eps=0.001, momentum=0.1, affine=True), 
+            nn.BatchNorm3d(128*2, eps=0.001, momentum=0.1, affine=True), 
             nn.ReLU(inplace=True)
         )
-        self.conv3 = nn.Conv3d(in_channels=1, out_channels=24, padding=(0, 0, 0),
-                               kernel_size=(3, 3, 128), stride=(1, 1, 1))
+
+        self.conv3 = MinMaxCNNLayer(1, 24, paddinglength=(0, 0, 0),kernelsize=(3, 3, 128), stride=(1, 1, 1))
+
         self.batch_norm3 = nn.Sequential(
-            nn.BatchNorm3d(24, eps=0.001, momentum=0.1, affine=True), 
+            nn.BatchNorm3d(24*2, eps=0.001, momentum=0.1, affine=True), 
             nn.ReLU(inplace=True)
         )
 
         self.avg_pooling = nn.AvgPool3d(kernel_size=(5, 5, 1))
         self.full_connection = nn.Sequential(
             # nn.Dropout(p=0.5),
-            nn.Linear(24, classes)  # ,
+            nn.Linear(6192, classes)  # ,
             # nn.Softmax()
         )
 
@@ -126,10 +78,11 @@ class SSRN_network(nn.Module):
         x2 = self.batch_norm2(self.conv2(x2))
         x2 = x2.permute(0, 4, 2, 3, 1)
         x2 = self.batch_norm3(self.conv3(x2))
+        
 
         x3 = self.res_net3(x2)
         x3 = self.res_net4(x3)
         x4 = self.avg_pooling(x3)
         x4 = x4.view(x4.size(0), -1)
-        # print(x10.shape)
+        
         return self.full_connection(x4)
