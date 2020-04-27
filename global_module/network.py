fe7e0012f3c5dc78a15597008ae6ffaeea2bac2d
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-04-27 11:34:22 +0530

    Update network.py

--- global_module/network.py
+++ global_module/network.py
@@ -407,6 +407,70 @@ class DBDA_Separable_network(nn.Module):
         # output = self.fc(x_pre)
         return output
 
+    
+    
+class CDCNN_gabor(nn.Module):
+    def __init__(self, band, classes):
+        super(CDCNN_gabor, self).__init__()
+        self.name = 'CDCNN'
+
+        self.conv11 = nn.Sequential(
+            GaborConv2d(in_channels=band, out_channels=128, kernel_size=(1, 1), device=device),
+            nn.MaxPool2d(kernel_size=(5, 5))
+        )
+        self.conv12 = nn.Sequential(
+            GaborConv2d(in_channels=band, out_channels=128, kernel_size=(3, 3), device=device),
+            nn.MaxPool2d(kernel_size=(3, 3))
+        )
+        self.conv13 = GaborConv2d(in_channels=band, out_channels=128, kernel_size=(5, 5), device=device)
+
+        self.batch_normal1 = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128, eps=0.001, momentum=0.1, affine=True)
+        )
+        self.conv2 = GaborConv2d(in_channels=384, out_channels=128, kernel_size=(1, 1), device=device)
+        self.res_net1 = Residual_2D(128, 128, (1, 1), (0, 0), batch_normal=True)
+        self.res_net2 = Residual_2D(128, 128, (1, 1), (0, 0))
+
+        self.conv3 = nn.Sequential(
+            nn.ReLU(),
+            GaborConv2d(in_channels=128, out_channels=128, kernel_size=(1, 1), device=device)
+        )
+        self.conv4 = nn.Sequential(
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            GaborConv2d(in_channels=128, out_channels=128, kernel_size=(1, 1), device=device)
+        )
+        self.conv5 = nn.Sequential(
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            GaborConv2d(in_channels=128, out_channels=128, kernel_size=(1, 1), device=device)
+        )
+
+        
+        self.full_connection = nn.Sequential(
+            nn.Linear(128, classes)
+            #nn.Sigmoid()
+        )
+
+    def forward(self, X):
+        X = X.squeeze(1).permute(0, 3, 1, 2)
+        x11 = self.conv11(X)
+        x12 = self.conv12(X)
+        x13 = self.conv13(X)
+        # print(x11.shape)
+        # print(x12.shape)
+        # print(x13.shape)
+        x1 = torch.cat((x11, x12, x13), dim=1)
+        x1 = self.conv2(x1)
+        x1 = self.res_net1(x1)
+        x1 = self.res_net2(x1)
+        x1 = self.conv3(x1)
+        x1 = self.conv4(x1)
+        x1 = self.conv5(x1)
+        x1 = x1.view(x1.shape[0], -1)
+        # print(x1.shape)
+        return self.full_connection(x1)
 
 class DBMA_network(nn.Module):
     def __init__(self, band, classes):
