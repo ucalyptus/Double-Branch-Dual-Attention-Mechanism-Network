b6c8398d1a8a67f795d3898b3bb54de5b204e026
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-04 19:27:33 +0530

    Update minmax.py

--- global_module/minmax.py
+++ global_module/minmax.py
@@ -20,7 +20,7 @@ class Residual(nn.Module):
             MinMaxCNNLayer(in_channels,out_channels,kernelsize=kernel_size,paddinglength=padding,stride=stride),
             nn.ReLU()
         )
-        self.conv2 = MinMaxCNNLayer(out_channels,out_channels,kernelsize=kernel_size,paddinglength=padding,stride=stride)
+        self.conv2 = MinMaxCNNLayer(out_channels*2,out_channels*2,kernelsize=kernel_size,paddinglength=padding,stride=stride)
         #nn.Conv3d(out_channels, out_channels,kernel_size=kernel_size, padding=padding,stride=stride)
         if use_1x1conv:
             self.conv3 = MinMaxCNNLayer(in_channels,out_channels,kernelsize=1,stride=stride,paddinglength=0)
@@ -28,7 +28,7 @@ class Residual(nn.Module):
         else:
             self.conv3 = None
         self.bn1 = nn.BatchNorm3d(out_channels*2)  #*2 was modification
-        self.bn2 = nn.BatchNorm3d(out_channels*2)
+        self.bn2 = nn.BatchNorm3d(out_channels*2*2)
 
     def forward(self, X):
         Y = F.relu(self.bn1(self.conv1(X)))
