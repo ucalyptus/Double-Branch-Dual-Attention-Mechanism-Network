2e1c5c09c7fb67b935debceaa74821223a32424b
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-11 09:43:55 +0000

    d

--- global_module/generate_pic.py
+++ global_module/generate_pic.py
@@ -10,68 +10,9 @@ import torch.utils.data as Data
 
 import ast
 import os
-mydir = "/content/Double-Branch-Dual-Attention-Mechanism-Network/SSRN/"
-for file in os.listdir(mydir):
-    if file.endswith(".txt"):
-        pass
-        #print(file)
-print("Full")
-print("Test-Model")
 
+split = float(input("Enter Validation Split"))
 
-allband=False
-filename = input("Enter filename ")
-if filename=="Test-Model":
-    allband=True
-    split=0.95
-else:
-    split = float(input("Enter VALIDATION_SPLIT ")) 
-    if split > 1.00 or split <= 0.05:
-        print("Split was wrong, defaulting to 0.95")
-        split=0.95
-    if filename=="200":
-        allband=True
-    else:
-        nbands = int(input("Select Number of bands "))
-
-        with open(filename, 'r') as f:
-            BANDLIST = ast.literal_eval(f.read())
-
-            if nbands>len(BANDLIST) or nbands<5:
-                print("u entered more bands than provided in the bandlist.")
-                exit()
-    
-    
-def pavia_transform(ARRAY,BANDLIST):
-    if BANDLIST is not None:
-        BANDLIST=BANDLIST[:15]
-        BANDLIST = BANDLIST[:nbands]
-        assert ARRAY.shape[2] ==103
-        tensor_list = []
-        for i in range(0,len(BANDLIST)):
-            tensor_list.append(ARRAY[:,:,BANDLIST[i]])
-        return np.stack(tensor_list,axis=2)
-
-def salinas_transform(ARRAY,BANDLIST):
-    if BANDLIST is not None:
-        BANDLIST=BANDLIST[:20]    
-        BANDLIST = BANDLIST[:nbands]
-        assert ARRAY.shape[2] ==204
-        tensor_list = []
-        for i in range(0,len(BANDLIST)):
-            tensor_list.append(ARRAY[:,:,BANDLIST[i]])
-        return np.stack(tensor_list,axis=2)
-
-
-def indian_transform(ARRAY,BANDLIST):
-    if BANDLIST is not None:
-        BANDLIST=BANDLIST[:25]    
-        BANDLIST = BANDLIST[:nbands]
-        assert ARRAY.shape[2] ==200
-        tensor_list = []
-        for i in range(0,len(BANDLIST)):
-            tensor_list.append(ARRAY[:,:,BANDLIST[i]])
-        return np.stack(tensor_list,axis=2)
 
 def load_dataset(Dataset):
     if Dataset == 'IN':
@@ -79,8 +20,6 @@ def load_dataset(Dataset):
         mat_gt = sio.loadmat('../datasets/Indian_pines_gt.mat')
         data_hsi = mat_data['indian_pines_corrected']
         gt_hsi = mat_gt['indian_pines_gt']
-        if not allband:
-            data_hsi = indian_transform(data_hsi,BANDLIST)
         TOTAL_SIZE = 10249
         VALIDATION_SPLIT = split
         TRAIN_SIZE = math.ceil(TOTAL_SIZE * VALIDATION_SPLIT)
@@ -90,7 +29,6 @@ def load_dataset(Dataset):
         gt_uPavia = sio.loadmat('../datasets/PaviaU_gt.mat')
         data_hsi = uPavia['paviaU']
         gt_hsi = gt_uPavia['paviaU_gt']
-        data_hsi = pavia_transform(data_hsi,BANDLIST)
         TOTAL_SIZE = 42776
         VALIDATION_SPLIT = split
         TRAIN_SIZE = math.ceil(TOTAL_SIZE * VALIDATION_SPLIT)
@@ -100,11 +38,10 @@ def load_dataset(Dataset):
         gt_SV = sio.loadmat('../datasets/Salinas_gt.mat')
         data_hsi = SV['salinas_corrected']
         gt_hsi = gt_SV['salinas_gt']
-        data_hsi = salinas_transform(data_hsi,BANDLIST)
         TOTAL_SIZE = 54129
         VALIDATION_SPLIT = split
         TRAIN_SIZE = math.ceil(TOTAL_SIZE * VALIDATION_SPLIT)
-    
+    filename="Full"
     return data_hsi, gt_hsi, TOTAL_SIZE, TRAIN_SIZE, VALIDATION_SPLIT,filename
 
 def save_cmap(img, cmap, fname):
