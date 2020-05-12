e0e1ef1faa15b962b69102270390c46f044d930c
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-12 11:54:16 +0530

    Update lee_indian.py

--- Lee/lee_indian.py
+++ Lee/lee_indian.py
@@ -35,6 +35,7 @@ print(data_hsi.shape)
 image_x, image_y, BAND = data_hsi.shape
 data = data_hsi.reshape(np.prod(data_hsi.shape[:2]), np.prod(data_hsi.shape[2:]))
 gt = gt_hsi.reshape(np.prod(gt_hsi.shape[:2]),)
+print(gt.shape)
 CLASSES_NUM = max(gt)
 print('The class numbers of the HSI data is:', CLASSES_NUM)
 
