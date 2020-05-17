c3c75504148d5243e467450f5f7b9f42ad997ca1
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-17 07:06:10 +0000

    changes1

--- global_module/network.py
+++ global_module/network.py
@@ -12,90 +12,6 @@ import sys
 sys.path.append('../global_module/')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-class LeeEtAl(nn.Module):
-    """
-    CONTEXTUAL DEEP CNN BASED HYPERSPECTRAL CLASSIFICATION
-    Hyungtae Lee and Heesung Kwon
-    IGARSS 2016
-    """
-    @staticmethod
-    def weight_init(m):
-        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv3d):
-            init.kaiming_uniform_(m.weight)
-            init.zeros_(m.bias)
-
-    def __init__(self, in_channels, n_classes):
-        super(LeeEtAl, self).__init__()
-        # The first convolutional layer applied to the input hyperspectral
-        # image uses an inception module that locally convolves the input
-        # image with two convolutional filters with different sizes
-        # (1x1xB and 3x3xB where B is the number of spectral bands)
-        self.conv_3x3 = nn.Conv3d(
-            1, 128, ( 3, 3,in_channels), stride=(1, 1, 2), padding=(1,1,0))
-        self.conv_1x1 = nn.Conv3d(
-            1, 128, ( 1, 1,in_channels), stride=(1, 1, 1), padding=0)
-        self.name = 'LeeEtAl'
-
-        # We use two modules from the residual learning approach
-        # Residual block 1
-        self.conv1 = nn.Conv2d(256, 128, (1, 1))
-        self.conv2 = nn.Conv2d(128, 128, (1, 1))
-        self.conv3 = nn.Conv2d(128, 128, (1, 1))
-
-        # Residual block 2
-        self.conv4 = nn.Conv2d(128, 128, (1, 1))
-        self.conv5 = nn.Conv2d(128, 128, (1, 1))
-
-        # The layer combination in the last three convolutional layers
-        # is the same as the fully connected layers of Alexnet
-        self.conv6 = nn.Conv2d(128, 128, (1, 1))
-        self.conv7 = nn.Conv2d(128, 128, (1, 1))
-        self.conv8 = nn.Conv2d(128, n_classes, (9, 9))
-
-        self.lrn1 = nn.LocalResponseNorm(256)
-        self.lrn2 = nn.LocalResponseNorm(128)
-
-        # The 7 th and 8 th convolutional layers have dropout in training
-        self.dropout = nn.Dropout(p=0.5)
-        
-        self.apply(self.weight_init)
-
-    def forward(self, x):
-        # Inception module
-        x_3x3 = self.conv_3x3(x)
-        x_1x1 = self.conv_1x1(x)
-        x = torch.cat([x_3x3, x_1x1], dim=1)
-        # Remove the third dimension of the tensor
-        x = torch.squeeze(x)
-
-        # Local Response Normalization
-        x = F.relu(self.lrn1(x))
-
-        # First convolution
-        x = self.conv1(x)
-
-        # Local Response Normalization
-        x = F.relu(self.lrn2(x))
-
-        # First residual block
-        x_res = F.relu(self.conv2(x))
-        x_res = self.conv3(x_res)
-        x = F.relu(x + x_res)
-
-        # Second residual block
-        x_res = F.relu(self.conv4(x))
-        x_res = self.conv5(x_res)
-        x = F.relu(x + x_res)
-
-        x = F.relu(self.conv6(x))
-        x = self.dropout(x)
-        x = F.relu(self.conv7(x))
-        x = self.dropout(x)
-        x = self.conv8(x)
-        x = x.squeeze(2).squeeze(2)
-        return x
 class ChenEtAl(nn.Module):
     """
     DEEP FEATURE EXTRACTION AND CLASSIFICATION OF HYPERSPECTRAL IMAGES BASED ON
@@ -111,7 +27,7 @@ class ChenEtAl(nn.Module):
             init.normal_(m.weight, std=0.001)
             init.zeros_(m.bias)
 
-    def __init__(self, input_channels, n_classes, patch_size=27, n_planes=32):
+    def __init__(self, input_channels, n_classes, patch_size=9, n_planes=32):
         super(ChenEtAl, self).__init__()
         self.input_channels = input_channels
         self.n_planes = n_planes
@@ -120,9 +36,9 @@ class ChenEtAl(nn.Module):
 
         self.conv1 = nn.Conv3d(1, n_planes, (32, 4, 4))
         self.pool1 = nn.MaxPool3d((1, 2, 2))
-        self.conv2 = nn.Conv3d(n_planes, n_planes, (32, 4, 4))
+        self.conv2 = nn.Conv3d(n_planes, n_planes, (32, 2, 2))
         self.pool2 = nn.MaxPool3d((1, 2, 2))
-        self.conv3 = nn.Conv3d(n_planes, n_planes, (32, 4, 4))
+        self.conv3 = nn.Conv3d(n_planes, n_planes, (32, 1, 1))
 
         self.features_size = self._get_final_flattened_size()
 
@@ -143,14 +59,21 @@ class ChenEtAl(nn.Module):
         return t * c * w * h
 
     def forward(self, x):
+        
         x = F.relu(self.conv1(x))
+        
         x = self.pool1(x)
+        
         x = self.dropout(x)
         x = F.relu(self.conv2(x))
+        
         x = self.pool2(x)
+        
         x = self.dropout(x)
         x = F.relu(self.conv3(x))
+        
         x = self.dropout(x)
         x = x.view(-1, self.features_size)
+        
         x = self.fc(x)
         return x
--- model_test.py
+++ model_test.py
@@ -12,88 +12,72 @@ sys.path.append('../global_module/')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class LeeEtAl(nn.Module):
+class ChenEtAl(nn.Module):
     """
-    CONTEXTUAL DEEP CNN BASED HYPERSPECTRAL CLASSIFICATION
-    Hyungtae Lee and Heesung Kwon
-    IGARSS 2016
+    DEEP FEATURE EXTRACTION AND CLASSIFICATION OF HYPERSPECTRAL IMAGES BASED ON
+                        CONVOLUTIONAL NEURAL NETWORKS
+    Yushi Chen, Hanlu Jiang, Chunyang Li, Xiuping Jia and Pedram Ghamisi
+    IEEE Transactions on Geoscience and Remote Sensing (TGRS), 2017
     """
     @staticmethod
     def weight_init(m):
+        # In the beginning, the weights are randomly initialized
+        # with standard deviation 0.001
         if isinstance(m, nn.Linear) or isinstance(m, nn.Conv3d):
-            init.kaiming_uniform_(m.weight)
+            init.normal_(m.weight, std=0.001)
             init.zeros_(m.bias)
 
-    def __init__(self, in_channels, n_classes):
-        super(LeeEtAl, self).__init__()
-        # The first convolutional layer applied to the input hyperspectral
-        # image uses an inception module that locally convolves the input
-        # image with two convolutional filters with different sizes
-        # (1x1xB and 3x3xB where B is the number of spectral bands)
-        self.conv_3x3 = nn.Conv3d(
-            1, 128, ( 3, 3,in_channels), stride=(1, 1, 2), padding=(1,1,0))
-        self.conv_1x1 = nn.Conv3d(
-            1, 128, ( 1, 1,in_channels), stride=(1, 1, 1), padding=0)
+    def __init__(self, input_channels, n_classes, patch_size=9, n_planes=32):
+        super(ChenEtAl, self).__init__()
+        self.input_channels = input_channels
+        self.n_planes = n_planes
+        self.patch_size = patch_size
+        self.name = 'ChenEtAl'
 
-        # We use two modules from the residual learning approach
-        # Residual block 1
-        self.conv1 = nn.Conv2d(256, 128, (1, 1))
-        self.conv2 = nn.Conv2d(128, 128, (1, 1))
-        self.conv3 = nn.Conv2d(128, 128, (1, 1))
+        self.conv1 = nn.Conv3d(1, n_planes, (32, 4, 4))
+        self.pool1 = nn.MaxPool3d((1, 2, 2))
+        self.conv2 = nn.Conv3d(n_planes, n_planes, (32, 2, 2))
+        self.pool2 = nn.MaxPool3d((1, 2, 2))
+        self.conv3 = nn.Conv3d(n_planes, n_planes, (32, 1, 1))
 
-        # Residual block 2
-        self.conv4 = nn.Conv2d(128, 128, (1, 1))
-        self.conv5 = nn.Conv2d(128, 128, (1, 1))
+        self.features_size = self._get_final_flattened_size()
 
-        # The layer combination in the last three convolutional layers
-        # is the same as the fully connected layers of Alexnet
-        self.conv6 = nn.Conv2d(128, 128, (1, 1))
-        self.conv7 = nn.Conv2d(128, 128, (1, 1))
-        self.conv8 = nn.Conv2d(128, n_classes, (9, 9))
+        self.fc = nn.Linear(self.features_size, n_classes)
 
-        self.lrn1 = nn.LocalResponseNorm(256)
-        self.lrn2 = nn.LocalResponseNorm(128)
-
-        # The 7 th and 8 th convolutional layers have dropout in training
         self.dropout = nn.Dropout(p=0.5)
-        
+
         self.apply(self.weight_init)
 
+    def _get_final_flattened_size(self):
+        with torch.no_grad():
+            x = torch.zeros((1, 1, self.input_channels,
+                             self.patch_size, self.patch_size))
+            x = self.pool1(self.conv1(x))
+            x = self.pool2(self.conv2(x))
+            x = self.conv3(x)
+            _, t, c, w, h = x.size()
+        return t * c * w * h
+
     def forward(self, x):
-        # Inception module
-        x_3x3 = self.conv_3x3(x)
-        x_1x1 = self.conv_1x1(x)
-        x = torch.cat([x_3x3, x_1x1], dim=1)
-        # Remove the third dimension of the tensor
-        x = torch.squeeze(x)
-
-        # Local Response Normalization
-        x = F.relu(self.lrn1(x))
-
-        # First convolution
-        x = self.conv1(x)
-
-        # Local Response Normalization
-        x = F.relu(self.lrn2(x))
-
-        # First residual block
-        x_res = F.relu(self.conv2(x))
-        x_res = self.conv3(x_res)
-        x = F.relu(x + x_res)
-
-        # Second residual block
-        x_res = F.relu(self.conv4(x))
-        x_res = self.conv5(x_res)
-        x = F.relu(x + x_res)
-
-        x = F.relu(self.conv6(x))
+        
+        x = F.relu(self.conv1(x))
+        
+        x = self.pool1(x)
+        
         x = self.dropout(x)
-        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv2(x))
+        
+        x = self.pool2(x)
+        
         x = self.dropout(x)
-        x = self.conv8(x)
-        x = x.squeeze(2).squeeze(2)
+        x = F.relu(self.conv3(x))
+        
+        x = self.dropout(x)
+        x = x.view(-1, self.features_size)
+        
+        x = self.fc(x)
         return x
 
-net = LeeEtAl(200,16).to(device)
+net = ChenEtAl(200,16).to(device)
 from torchsummary import summary
-print(summary(net,(1,9,9,200),batch_size=16))
+print(summary(net,(1,200,9,9),batch_size=16))
