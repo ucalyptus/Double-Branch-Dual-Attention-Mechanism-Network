2a7a5a852b809e1d3d91404b4595a2c3c16ea3d8
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-17 07:08:12 +0000

    changes2

--- global_module/network.py
+++ global_module/network.py
@@ -1,9 +1,6 @@
 import torch
 from torch import nn
-
 import math
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
 import numpy as np
 import torch.nn.functional as F
 import torch.nn.init as init
