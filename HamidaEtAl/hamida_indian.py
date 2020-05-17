a30e456485594d27ef09f5f924ae9a9f527e9fb2
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-17 09:08:01 +0000

    hamidamofo13

--- HamidaEtAl/hamida_indian.py
+++ HamidaEtAl/hamida_indian.py
@@ -71,7 +71,7 @@ padded_data = np.lib.pad(whole_data, ((PATCH_LENGTH, PATCH_LENGTH), (PATCH_LENGT
 
                          'constant', constant_values=0)
 net = network.HamidaEtAl(BAND, CLASSES_NUM).to(device)
-summary(net,input_size=(1,img_rows,img_cols,BAND))
+summary(net,input_size=(1,BAND,img_rows,img_cols))
 for index_iter in range(ITER):
     print(f"ITER : {index_iter+1}")
     
