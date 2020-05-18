283b9bbbf5207ed8124f200d0e841f6d7e76bea4
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-18 11:11:02 +0000

    456

--- global_module/network.py
+++ global_module/network.py
@@ -185,21 +185,19 @@ class Baseline(nn.Module):
         if isinstance(m, nn.Linear):
             init.kaiming_normal_(m.weight)
             init.zeros_(m.bias)
-
     def __init__(self, input_channels, n_classes, dropout=False):
         super(Baseline, self).__init__()
         self.use_dropout = dropout
         if dropout:
             self.dropout = nn.Dropout(p=0.5)
-
         self.fc1 = nn.Linear(input_channels, 2048)
         self.fc2 = nn.Linear(2048, 4096)
         self.fc3 = nn.Linear(4096, 2048)
         self.fc4 = nn.Linear(2048, n_classes)
-
         self.apply(self.weight_init)
 
     def forward(self, x):
+        x = x.permute(0,1,3,4,2)
         x = F.relu(self.fc1(x))
         if self.use_dropout:
             x = self.dropout(x)
--- model_test.py
+++ model_test.py
@@ -11,91 +11,41 @@ import sys
 sys.path.append('../global_module/')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-class HamidaEtAl(nn.Module):
+
+class Baseline(nn.Module):
     """
-    3-D Deep Learning Approach for Remote Sensing Image Classification
-    Amina Ben Hamida, Alexandre Benoit, Patrick Lambert, Chokri Ben Amar
-    IEEE TGRS, 2018
-    https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=8344565
+    Baseline network
     """
     @staticmethod
     def weight_init(m):
-        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv3d):
+        if isinstance(m, nn.Linear):
             init.kaiming_normal_(m.weight)
             init.zeros_(m.bias)
-
-    def __init__(self, input_channels, n_classes, patch_size=11, dilation=1):
-        super(HamidaEtAl, self).__init__()
-        # The first layer is a (3,3,3) kernel sized Conv characterized
-        # by a stride equal to 1 and number of neurons equal to 20
-        self.patch_size = patch_size
-        self.name = 'HamidaEtAl'
-        self.input_channels = input_channels
-        dilation = (dilation, 1, 1)
-
-        if patch_size == 3:
-            self.conv1 = nn.Conv3d(
-                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=1)
-        else:
-            self.conv1 = nn.Conv3d(
-                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=0)
-        # Next pooling is applied using a layer identical to the previous one
-        # with the difference of a 1D kernel size (1,1,3) and a larger stride
-        # equal to 2 in order to reduce the spectral dimension
-        self.pool1 = nn.Conv3d(
-            20, 20, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
-        # Then, a duplicate of the first and second layers is created with
-        # 35 hidden neurons per layer.
-        self.conv2 = nn.Conv3d(
-            20, 35, (3, 3, 3), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
-        self.pool2 = nn.Conv3d(
-            35, 35, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
-        # Finally, the 1D spatial dimension is progressively reduced
-        # thanks to the use of two Conv layers, 35 neurons each,
-        # with respective kernel sizes of (1,1,3) and (1,1,2) and strides
-        # respectively equal to (1,1,1) and (1,1,2)
-        self.conv3 = nn.Conv3d(
-            35, 35, (3, 1, 1), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
-        self.conv4 = nn.Conv3d(
-            35, 35, (2, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
-        #self.dropout = nn.Dropout(p=0.5)
-
-        self.features_size = self._get_final_flattened_size()
-        # The architecture ends with a fully connected layer where the number
-        # of neurons is equal to the number of input classes.
-        self.fc = nn.Linear(self.features_size, n_classes)
-        
-
+    def __init__(self, input_channels, n_classes, dropout=False):
+        super(Baseline, self).__init__()
+        self.use_dropout = dropout
+        if dropout:
+            self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(input_channels, 2048)
+        self.fc2 = nn.Linear(2048, 4096)
+        self.fc3 = nn.Linear(4096, 2048)
+        self.fc4 = nn.Linear(2048, n_classes)
         self.apply(self.weight_init)
 
-    def _get_final_flattened_size(self):
-        with torch.no_grad():
-            x = torch.zeros((1, 1, self.input_channels,
-                             self.patch_size, self.patch_size))
-            x = self.pool1(self.conv1(x))
-            x = self.pool2(self.conv2(x))
-            x = self.conv3(x)
-            x = self.conv4(x)
-            _, t, c, w, h = x.size()
-        return t * c * w * h
-
     def forward(self, x):
-        
-        x = F.relu(self.conv1(x))
-        
-        x = self.pool1(x)
-        
-        x = F.relu(self.conv2(x))
-        
-        x = self.pool2(x)
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = x.view(-1, self.features_size)
-        
-        
-        x = self.fc(x)
-        
+        x = x.permute(0,1,3,4,2)
+        x = F.relu(self.fc1(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = F.relu(self.fc3(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = self.fc4(x)
         return x
-net = HamidaEtAl(20,16).to(device)
+
+net = Baseline(20,16).to(device)
 from torchsummary import summary
 print(summary(net,(1,20,11,11),batch_size=16))
