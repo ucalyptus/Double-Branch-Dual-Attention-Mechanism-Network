408655f9e8eba783d222c8c6ebe4860f05e1803b
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-19 05:49:30 +0000

    djdj

--- model_test.py
+++ model_test.py
@@ -12,45 +12,88 @@ sys.path.append('../global_module/')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class LeNet5(torch.nn.Module):          
-     
-    def __init__(self,n_band,n_classes):     
-        super(LeNet5, self).__init__()
-        # Convolution (In LeNet-5, 32x32 images are given as input. Hence padding of 2 is done below)
-        self.conv1 = torch.nn.Conv2d(in_channels=n_band, out_channels=6, kernel_size=5, stride=1, padding=2, bias=True)
-        # Max-pooling
-        self.max_pool_1 = torch.nn.MaxPool2d(kernel_size=2)
-        # Convolution
-        self.conv2 = torch.nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=0, bias=True)
-        # Max-pooling
-        self.max_pool_2 = torch.nn.MaxPool2d(kernel_size=2) 
-        # Fully connected layer
-        self.fc1 = torch.nn.Linear(32, 120)   # convert matrix with 16*5*5 (= 400) features to a matrix of 120 features (columns)
-        self.fc2 = torch.nn.Linear(120, 84)       # convert matrix with 120 features to a matrix of 84 features (columns)
-        self.fc3 = torch.nn.Linear(84, n_classes)        # convert matrix with 84 features to a matrix of 10 features (columns)
+
+class HamidaEtAlmodified(nn.Module):
+    """
+    3-D Deep Learning Approach for Remote Sensing Image Classification
+    Amina Ben Hamida, Alexandre Benoit, Patrick Lambert, Chokri Ben Amar
+    IEEE TGRS, 2018
+    https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=8344565
+    """
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv3d):
+            init.kaiming_normal_(m.weight)
+            init.zeros_(m.bias)
+
+    def __init__(self, input_channels, n_classes, patch_size=11, dilation=1):
+        super(HamidaEtAlmodified, self).__init__()
+        # The first layer is a (3,3,3) kernel sized Conv characterized
+        # by a stride equal to 1 and number of neurons equal to 20
+        self.patch_size = patch_size
+        self.name = 'HamidaEtAlmodified'
+        self.input_channels = input_channels
+        dilation = (dilation, 1, 1)
+
+        if patch_size == 3:
+            self.conv1 = nn.Conv3d(
+                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=1)
+        else:
+            self.conv1 = nn.Conv3d(
+                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=0)
+        # Next pooling is applied using a layer identical to the previous one
+        # with the difference of a 1D kernel size (1,1,3) and a larger stride
+        # equal to 2 in order to reduce the spectral dimension
+        self.pool1 = nn.Conv3d(
+            20, 20, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
+        # Then, a duplicate of the first and second layers is created with
+        # 35 hidden neurons per layer.
+        self.conv2 = nn.Conv3d(
+            20, 35, (3, 3, 3), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
+        self.pool2 = nn.Conv3d(
+            35, 35, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
+        # Finally, the 1D spatial dimension is progressively reduced
+        # thanks to the use of two Conv layers, 35 neurons each,
+        # with respective kernel sizes of (1,1,3) and (1,1,2) and strides
+        # respectively equal to (1,1,1) and (1,1,2)
         
+        #self.dropout = nn.Dropout(p=0.5)
+
+        self.features_size = self._get_final_flattened_size()
+        # The architecture ends with a fully connected layer where the number
+        # of neurons is equal to the number of input classes.
+        self.fc = nn.Linear(self.features_size, n_classes)
+        
+
+        self.apply(self.weight_init)
+
+    def _get_final_flattened_size(self):
+        with torch.no_grad():
+            x = torch.zeros((1, 1, self.input_channels,
+                             self.patch_size, self.patch_size))
+            x = self.pool1(self.conv1(x))
+            x = self.pool2(self.conv2(x))
+            
+            _, t, c, w, h = x.size()
+        return t * c * w * h
+
     def forward(self, x):
-        # convolve, then perform ReLU non-linearity
-        x = torch.nn.functional.relu(self.conv1(x))
-        # max-pooling with 2x2 grid 
-        x = self.max_pool_1(x) 
-        # convolve, then perform ReLU non-linearity
-        x = torch.nn.functional.relu(self.conv2(x))
-        # max-pooling with 2x2 grid
-        x = self.max_pool_2(x)
-        # first flatten 'max_pool_2_out' to contain 16*5*5 columns
-        # read through https://stackoverflow.com/a/42482819/7551231
-        x = x.view(-1, 32)
-        # FC-1, then perform ReLU non-linearity
-        x = torch.nn.functional.relu(self.fc1(x))
-        # FC-2, then perform ReLU non-linearity
-        x = torch.nn.functional.relu(self.fc2(x))
-        # FC-3
-        x = self.fc3(x)
+        
+        x = F.relu(self.conv1(x))
+        
+        x = self.pool1(x)
+        
+        x = F.relu(self.conv2(x))
+        
+        x = self.pool2(x)
+        x = x.view(-1, self.features_size)
+        
+        
+        x = self.fc(x)
         
         return x
 
 
-net = LeNet5(200,16).to(device)
+net = HamidaEtAlmodified(200,16).to(device)
 from torchsummary import summary
-print(summary(net,(200,11,11),batch_size=16))
+print(summary(net,(1,200,11,11),batch_size=16))
